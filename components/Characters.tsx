9c0914a9bf0b1abd83b05714d04c9ac5be276a27
timv <>
2022-06-24 15:32:20 +0200

    highlight board tile

--- components/Characters.tsx
+++ components/Characters.tsx
@@ -1,21 +1,25 @@
 import styles from '../styles/Shmurgle.module.css';
-import { WORD_LENGTH } from './Shmurgle';
+import { VALID_WORD_LENGTH } from './Shmurgle';
+import cx from 'classnames';
 
-function Characters({ input }: { input: string }) {
+function Characters({ input, current }: { input: string; current: boolean }) {
     let chars: string[] = [];
-    let count: number = 0;
 
     if (input.length > 0) {
         chars = input.split('');
     }
-    while (chars.length < WORD_LENGTH) {
+    while (chars.length < VALID_WORD_LENGTH) {
         chars.push('');
     }
+
+    const classNames = cx(styles.attempt_char, {
+        [styles.current]: current,
+    });
 
     return (
         <>
             {chars.map((char, i) => (
-                <span key={`${i}_${char}`} className={styles.attempt_char}>
+                <span key={`${i}_${char}`} className={classNames}>
                     {char}
                 </span>
             ))}
