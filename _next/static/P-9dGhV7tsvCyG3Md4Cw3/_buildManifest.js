4bb5048ea9b944d750359fae83ebbd966d06eeb6
timv <>
2022-06-24 16:42:22 +0200

    Updates

---
+++ _next/static/P-9dGhV7tsvCyG3Md4Cw3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-434e20cccc0ea2cf.js","static/css/afd88f1fc4d4f9b7.css","static/chunks/pages/index-b422abb0538aaaa1.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
