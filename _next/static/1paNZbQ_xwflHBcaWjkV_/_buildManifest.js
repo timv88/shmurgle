6f83d6e13348aa486a89ba06b645dcf08b129111
timv <t.verheijke@gmail.com>
2022-06-26 20:56:18 +0200

    Updates

---
+++ _next/static/1paNZbQ_xwflHBcaWjkV_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/562-434e20cccc0ea2cf.js","static/css/afd88f1fc4d4f9b7.css","static/chunks/pages/index-df1fab89808283c5.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
