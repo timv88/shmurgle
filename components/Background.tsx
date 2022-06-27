73088a21ef41795cb23f574ef233e3a605c5b937
timv <t.verheijke@gmail.com>
2022-06-27 11:14:42 +0200

    improve background colour steps

--- components/Background.tsx
+++ components/Background.tsx
@@ -3,7 +3,29 @@ import BackgroundWaves from './BackgroundWaves';
 import BackgroundChars from './BackgroundChars';
 import { gameStateType, previousAttempt } from './types';
 import styles from '../styles/Shmurgle.module.css';
-import { MAX_ATTEMPTS } from './Shmurgle';
+import { MAX_ATTEMPTS } from './constants';
+
+const { LOST, WON } = gameStateType;
+
+function getBackgroundColor(gameState: gameStateType, currentAttemptIdx: number) {
+    const baseColor = 'rgb(21,177,239)';
+    const loseColor = 'rgb(211, 47, 47)';
+    const winColor = 'rgb(0,192,118)';
+
+    let color = mix(
+        baseColor,
+        loseColor,
+        1 - currentAttemptIdx / (MAX_ATTEMPTS + 1)
+    )({});
+    
+    if (gameState === WON) {
+        color = winColor;
+    } else if (gameState === LOST) {
+        color = loseColor;
+    }
+
+    return color;
+}
 
 function Background({
     gameState,
@@ -18,20 +40,12 @@ function Background({
     previousAttempts: previousAttempt[];
     backgroundChars: string[];
 }) {
-    const { LOST, WON } = gameStateType;
-    const offsetY =
-        gameState === LOST || gameState === WON
-            ? `100vh`
-            : `${currentAttemptIdx * 12 + 30}vh`;
+    const offsetY = gameState === LOST || gameState === WON
+        ? `100vh`
+        : `${currentAttemptIdx * 12 + 30}vh`;
 
-    const primary = 'rgb(21,177,239)';
-    const lose = 'rgb(211, 47, 47)';
-    const win = 'rgb(0,192,118)';
+    const color = getBackgroundColor(gameState, currentAttemptIdx);
 
-    let color = mix(primary, lose, 1 - currentAttemptIdx / MAX_ATTEMPTS)({});
-    if (gameState === WON) {
-        color = win;
-    }
     return (
         <div className={styles.background_container}>
             <BackgroundWaves color={color} offsetY={offsetY} />
