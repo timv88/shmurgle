f69fc0e1b7364cfdc5ff204426a25592b44c084c
timv <t.verheijke@gmail.com>
2022-06-29 00:37:56 +0200

    Updates

---
+++ _next/static/an8WnF0lRvmubY45kpRV2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36-ebc48b234c315525.js","static/css/d4a56e352c24fc7e.css","static/chunks/pages/index-a199c3779a59acfa.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/uG53_yLj5tICDIQgZhrlJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36-ebc48b234c315525.js","static/css/d4a56e352c24fc7e.css","static/chunks/pages/index-745730b1bbe2f411.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
