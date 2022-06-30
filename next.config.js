ccd221b20915e3366d14da359cb9aa9b6bfd3d72
timv <t.verheijke@gmail.com>
2022-06-30 14:14:30 +0200

    add stylelint

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
 const isDev = process.env.NODE_ENV !== 'production';
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config, options) => {
+        config.plugins.push(new StylelintPlugin());
+        return config;
+    },
     basePath: isDev ? '' : '/shmurgle',
     assetPrefix: isDev ? '' : '/shmurgle/',
 };
